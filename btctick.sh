30e8746d0e709fa26566c5f933f55d0314dd4b15
kammoji <juhana@konekettu.fi>
2020-01-20 22:39:57 +0200

    Fixed main script for Coinmarketcap 2020 updates.

--- btctick.sh
+++ btctick.sh
@@ -31,21 +31,21 @@ check_update
 
 date=`date | sed 's/ /_/g'`
 
-wget -q --output-document coinmarketcap_data_$date\.html coinmarketcap.com
+wget -q --output-document coinmarketcap_data_$date.html coinmarketcap.com/currencies/bitcoin
 
 if [ -s "coinmarketcap_data_$date.html" ]
 then
-	grep -A 20 "href=\"/currencies/bitcoin/\">Bitcoin</a>" coinmarketcap_data_$date\.html > coinmarketcap_data_$date
-	cap=`grep -A 1 'market-cap' "coinmarketcap_data_$date" | tail -n 1`
+	#grep -A 20 "href=\"/currencies/bitcoin/\">Bitcoin</a>" coinmarketcap_data_$date\.html > coinmarketcap_data_$date
+	cap=`grep -o -P 'Market\sCap.{0,40}' coinmarketcap_data_$date\.html | cut -d">" -f 3 | cut -d"<" -f1 | cut -d" " -f 1`
 	#echo $cap
 	#cap_parsed=`printf "%.0f" $cap | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-	price=`grep 'price' coinmarketcap_data_$date | cut -d">" -f 2 | cut -d "<" -f 1`
-	volume=`grep 'volume' coinmarketcap_data_$date | cut -d">" -f 2 | cut -d "<" -f 1`
+	price=`grep -o -P 'Price.{0,30}' coinmarketcap_data_$date\.html | cut -d">" -f 3 | cut -d"<" -f1 | cut -d" " -f 1`
+	volume=`grep -o -P 'Volume\s.{0,60}' coinmarketcap_data_$date\.html | cut -d">" -f 4 | cut -d"<" -f1 | cut -d" " -f 1`
 	echo
 	echo "btctick.sh Bitcoin ticker - Copyleft Juhana Kammonen 2018"
 	echo
 	date
-	echo "Bitcoin market cap is: "$cap
+	echo "Bitcoin market cap is:"$cap
 	echo "Bitcoin average price across exchanges is: "$price
 	echo "Bitcoin trading volume (last 24h) is: "$volume
 	echo
@@ -55,7 +55,6 @@ then
 
 	#cleanup:
 	rm "coinmarketcap_data_$date.html"
-	rm "coinmarketcap_data_$date"
 
 else
 	echo
