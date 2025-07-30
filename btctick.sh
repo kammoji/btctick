3ed0b817d3dc68f3e2c110d03c7fdc918a88d2aa
kammoji <juhana@konekettu.fi>
2025-07-30 13:03:24 +0300

    Ending July updates and adjustments...

--- btctick.sh
+++ btctick.sh
@@ -28,7 +28,7 @@ for i in `seq 0 5`;
   		for j in "${spin[@]}"
   			do
         		echo -ne "\b$j"
-        		sleep 0.080
+        		sleep 0.085
 		done
 
 	done
@@ -84,10 +84,10 @@ then
 	echo "btctick.sh - "$'\u20bf'"itcoin market price ticker"
 	echo "----------"
 	echo
-	echo "You are at btctick.sh master branch edit 2025-06-19 - USD (\$)"
+	echo "You are at btctick.sh master branch edit 2025-07-30 - USD (\$)"
 	echo "btctick is public domain with NO WARRANTY. Use at your own discretion."
 	echo
-	echo "NEWS: Bitcoin market price around \$100k again. All aboard the "$'\u20bf'" train while it's still on the station!"
+	echo "NEWS: Bitcoin market price topping \$100k again. All aboard the "$'\u20bf'" train while it's still on the station!"
 	echo
 	date
 	echo "Bitcoin market cap: "\$$cap_parsed
