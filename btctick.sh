9520e13dac3f60c2746cda5d858f7e4e27679449
kammoji <juhana@konekettu.fi>
2026-03-18 21:11:16 +0200

    Spinner adjustments and daily patches...

--- btctick.sh
+++ btctick.sh
@@ -28,7 +28,7 @@ for i in `seq 0 5`;
   		for j in "${spin[@]}"
   			do
         		echo -ne "\b$j"
-        		sleep 0.082
+        		sleep 0.084
 		done
 
 	done
@@ -84,10 +84,10 @@ then
 	echo "btctick.sh - "$'\u20bf'"itcoin market price ticker"
 	echo "----------"
 	echo
-	echo "You are at btctick master branch edit 2026-03-16 - default FIAT is USD (\$)."
+	echo "You are at btctick master branch edit 2026-03-18 - default FIAT is USD (\$)."
 	echo "btctick is public domain with NO WARRANTY. Use at your own discretion."
 	echo
-	echo "NEWS: Happy "$'\u20bf'" bear year 2026! Start preparing for the next bull market in 3-4 years!"
+	echo "NEWS: Happy "$'\u20bf'" bear year 2026! Start preparing for the next big bull run in ca. 3 years!"
 	echo
 	date
 	echo "Bitcoin market cap: "\$$cap_parsed
