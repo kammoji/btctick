0577f660d2fa608c68dc631ff0bef723d24677d3
kammoji <juhana@konekettu.fi>
2026-04-13 19:45:32 +0300

    April updates, mainly cmarketcap fixes...

--- btctick.sh
+++ btctick.sh
@@ -28,7 +28,7 @@ for i in `seq 0 5`;
   		for j in "${spin[@]}"
   			do
         		echo -ne "\b$j"
-        		sleep 0.087
+        		sleep 0.089
 		done
 
 	done
@@ -70,11 +70,11 @@ if [ -s "coinmarketcap_data_$date.html" ] && [ ! -z "$FILEINFO" ]
 then
 	spinner
 	#grep -A 20 "href=\"/currencies/bitcoin/\">Bitcoin</a>" coinmarketcap_data_$date\.html > coinmarketcap_data_$date
-	cap=`cat coinmarketcap_data_$date\.html | grep -o -P 'Cap":.{0,500}' | head -n 1 | cut -d":" -f 2 | cut -d"," -f 1`
+	cap=`cat coinmarketcap_data_$date\.html | grep -o -P 'Cap":.{0,500}' | head -n 15 | tail -n 1 | cut -d":" -f 21 | cut -d"," -f 1`
 	#echo $cap
 	#cap_parsed=`printf "%.0f" $cap | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-	price=`cat coinmarketcap_data_$date\.html | grep -o -P 'price".{0,500}' | head -n 6  | tail -n 1 | cut -d":" -f 2 | cut -d"." -f 1`
-	volume=`cat coinmarketcap_data_$date\.html | grep -o -P 'volume".{0,40}' | tail -n 1 | cut -d"," -f 1 | cut -d":" -f 2`
+	price=`cat coinmarketcap_data_$date\.html | grep -o -P 'price".{0,500}' | head -n 15 | tail -n 1 | cut -d":" -f 2 | cut -d"." -f 1`
+	volume=`cat coinmarketcap_data_$date\.html | grep -o -P 'volume".{0,40}' | head -n 15 | tail -n 1 | cut -d"," -f 1 | cut -d":" -f 2`
 	#Enter parser:
 	cap_parsed=`printf "%.0f" $cap | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
 	price_parsed=`printf "%.0f" $price | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
@@ -84,7 +84,7 @@ then
 	echo "btctick.sh - "$'\u20bf'"itcoin market price ticker"
 	echo "----------"
 	echo
-	echo "You are at btctick master branch edit 2026-03-25 - default FIAT is USD (\$)."
+	echo "You are at btctick master branch edit 2026-04-13 - default FIAT is USD (\$)."
 	echo "btctick is public domain with NO WARRANTY. Use at your own discretion."
 	echo
 	echo "NEWS: Happy "$'\u20bf'" bear year 2026! Start preparing for the next bull run in ca. 3 years, it will be sooner than you think!"
