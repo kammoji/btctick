6748e240b773fbd036cd9305222fa8db290b4066
kammoji <juhana@konekettu.fi>
2026-06-29 15:07:42 +0300

    End of June updates...

--- btctick.sh
+++ btctick.sh
@@ -28,7 +28,7 @@ for i in `seq 0 5`;
   		for j in "${spin[@]}"
   			do
         		echo -ne "\b$j"
-        		sleep 0.08
+        		sleep 0.0795
 		done
 
 	done
@@ -84,10 +84,10 @@ then
 	echo "btctick.sh - "$'\u20bf'"itcoin market price ticker"
 	echo "----------"
 	echo
-	echo "You are at btctick master branch edit 2026-06-25 - default FIAT is USD (\$)."
+	echo "You are at btctick master branch edit 2026-06-29 - default FIAT is USD (\$)."
 	echo "btctick is public domain with NO WARRANTY. Use at your own discretion."
 	echo
-	echo "NEWS: Celebrating BTC Bear Summer 2026! Prepare for the next bull run in ca. 3 years! Why not start stacking your sats, visit https://konekettu.fi for opportunities."
+	echo "NEWS: Celebrating BTC Bear Summer 2026! Prepare for the next bull run in ca. 3 years! A bear market is a great chance to start stacking Satoshis, visit https://konekettu.fi for opportunities."
 	echo
 	date
 	echo "Bitcoin market cap: "\$$cap_parsed
