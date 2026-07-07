703f1e17bafedef1501587eed5ab96b48f3e45c3
kammoji <juhana@konekettu.fi>
2026-07-07 11:15:09 +0300

    Start of July updates...

--- btctick.sh
+++ btctick.sh
@@ -84,10 +84,10 @@ then
 	echo "btctick.sh - "$'\u20bf'"itcoin market price ticker"
 	echo "----------"
 	echo
-	echo "You are at btctick master branch edit 2026-06-29 - default FIAT is USD (\$)."
+	echo "You are at btctick master branch edit 2026-07-07 - default FIAT is USD (\$)."
 	echo "btctick is public domain with NO WARRANTY. Use at your own discretion."
 	echo
-	echo "NEWS: Celebrating BTC Bear Summer 2026! Prepare for the next bull run in ca. 3 years! A bear market is a great chance to start stacking Satoshis, visit https://konekettu.fi for opportunities."
+	echo "NEWS: Celebrating BTC Bear Summer and July 2026! Prepare for the next bull run in ca. 3 years! A bear market is a great chance to start stacking your Sats, visit https://konekettu.fi for opportunities."
 	echo
 	date
 	echo "Bitcoin market cap: "\$$cap_parsed
